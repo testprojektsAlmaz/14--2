15bdaee05daa873e7b4970c87c3338ac41ac95af
Almaz <tunerlag@gmail.com>
2022-05-11 14:38:39 +0300

    commit names

---
+++ main.js
@@ -0,0 +1,113 @@
+/*
+
+// Переменные
+
+
+const a = "a" // string
+ 
+const b = 20 // number
+ 
+const bool = true; // boolean
+
+const bool2 = false; // 
+
+console.log(typeof a)
+
+const obj = {
+    key: "value", 
+    username: "Alex",
+    data: {
+        first_name: "Jack",
+        last_name: "Jayson"
+
+    }
+}
+
+console.log(obj.data)  */
+
+//Массивы
+
+/*
+const arr = ["Alex", true, false, 34]
+
+console.log(arr[0])
+
+const arr2 = [
+    {
+        key: "value"
+    },
+    {
+        key: "value"
+    },
+    {
+        key: "value3"
+    },
+]
+console.log(arr2[2].key)  */
+
+/*
+const name = prompt("Как вас зовут?");
+
+console.log("Hello " + name)
+*/
+
+//const name = alert("Вам запрещено")
+
+const first_number = prompt("Введите число");
+const last_number = prompt("Введите второе число");
+
+if (first_number>last_number) {
+
+console.log(first_number + ">" + last_number)
+} else if (first_number < last_number){
+    console.log(first_number + "<" + last_number)
+} else {
+    console.log(first_number + "=" + last_number)
+}
+
+
+
+
+/*
+const a = "afad";
+
+let c = 235;
+
+ console.log(c)
+
+c = 23;
+
+
+console.log(c)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

e1ce3f5aa1f03c0c4c5423d804a7149adfe8e9e6
Almaz <tunerlag@gmail.com>
2022-05-12 00:56:03 +0300

    dz1

--- dz1/greeting.js
+++ dz1/greeting.js
@@ -1,5 +1,73 @@
 
-
+// Первое задание dz1. 
+/*
 const name = prompt("Как вас зовут?");
 
-console.log("Hello " + name)
+console.log("Hello " + name)  */
+
+
+// Второе задание dz1
+/*
+let arrOne = ["Эркин", "Гульмира", "Азамат", "Мирлан"];
+
+console.log(arrOne);
+
+console.log(arrOne.length); 
+
+let arrOne = [5, 9, 12, 17, 25, 39, 102, 32, 21];
+
+console.log(arrOne);
+
+console.log(arrOne.length); */
+
+// Третье задание dz1
+
+let color = "red";
+
+switch (color) {
+    case "red":
+        console.log("color is red");
+        break;
+
+    case "yellow":
+        console.log("color is yellow");
+        break;
+
+    case "green":
+        console.log("color is green");
+        break;
+}
+
+/*
+let color = "yellow";
+
+switch (color) {
+    case "red":
+        console.log("color is red");
+        break;
+
+    case "yellow":
+        console.log("color is yellow");
+        break;
+
+    case "green":
+        console.log("color is green");
+        break;
+}*/
+
+/*
+let color = "green";
+
+switch (color) {
+    case "red":
+        console.log("color is red");
+        break;
+
+    case "yellow":
+        console.log("color is yellow");
+        break;
+
+    case "green":
+        console.log("color is green");
+        break;
+} */

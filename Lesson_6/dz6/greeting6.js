5ed4455b6f6f0d98945c1947d796a16f086bc2d0
Almaz <tunerlag@gmail.com>
2022-05-28 10:25:17 +0300

    dz6

---
+++ Lesson_6/dz6/greeting6.js
@@ -0,0 +1,99 @@
+// Lesson-6 dz:
+
+// Задание № 1.
+
+/*
+const mass = [1,2,3,4,5]
+
+ const mass2 = [...mass]
+
+ for(let i = 0; i > mass.length; i++){
+     mass2.push(mass[i])
+ }
+
+console.log(mass2); 
+
+const obj =  {
+    username: "Richard",
+
+    last_name: "Smith",
+
+    age: 36,
+
+    full_name: "Richard Smith", 
+
+    Job: "Driver"
+
+}
+
+const obj2 = [{...obj}]
+
+console.log(obj2) 
+
+
+
+
+//Задание № 2.
+
+
+const obj =  {
+
+    key: "value",
+
+    key2: "value",
+
+    key3: "value", 
+
+    key4: "value",
+
+    key5: "value" 
+    
+}
+
+let obj2 = [{...obj}]
+
+
+const key = {
+    key6: "value",
+
+    key7: "value",
+
+    key8: "value"
+}
+
+
+console.log(obj2, key);
+
+
+
+
+// Задание № 3.
+
+
+document.querySelector('.btn').onclick = myClick;
+
+function myClick(){
+     let name = document.querySelector('.inp').value;
+     
+     document.querySelector('.out').innerHTML = "Hello" + " " + name;
+           
+
+     console.log("Hello " + name);
+     
+}  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
